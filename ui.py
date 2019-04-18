b52344c276dcb7aafcfb377f4e432f19cbc763a9
Lech Łończyński <llonczynski@gmail.com>
2019-04-18 20:17:55 +0100

    test

--- ui.py
+++ ui.py
@@ -1,15 +1,15 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMainWindow
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
-        return super().__init__()
-
-        self.setWindowTitle('Hid Generator')
+        super().__init__()
+        
         self.setGeometry(0, 0, 100, 100)  
               
         self.QLabel = QLabel()
-
+        
+        self.setLayout()
         self.initui()
 
     def initui(self):
