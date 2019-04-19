7d43f459d3cdca730bfc0ba5faebb079bb4bac9d
Lech Lonczynski <llonczynski@gmail.com>
2019-04-19 12:52:16 +0200

    Adding file browser.

---
+++ functions.py
@@ -0,0 +1,7 @@
+import unidecode
+import numpy
+import os
+
+class CardCreate():
+    def __init__(self):
+        pass
--- ui.py
+++ ui.py
@@ -1,17 +1,53 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QHBoxLayout, QVBoxLayout,
+                            QFileDialog, QPushButton, QListWidget)
 
 
 class MainWindow(QWidget):
     def __init__(self):
-        super().__init__()
+        super().__init__()         
         
-        self.setGeometry(0, 0, 100, 100)  
-              
-        self.QLabel = QLabel()
+        self.file = QFileDialog(self, )
+        self.label = QLabel('Test')
+        self.list = QListWidget()
+        self.btn = QPushButton('Browse files')
+        self.delbtn= QPushButton('Delete')
+        self.hbox = QHBoxLayout()
+        self.hbox2 = QHBoxLayout()
+        self.vbox = QVBoxLayout()        
         
-        self.setLayout()
         self.initui()
 
     def initui(self):
-                
+        self.hbox.addStretch()
+        self.hbox.addWidget(self.label)
+        self.hbox.addStretch()
+
+        self.hbox2.addStretch()
+        self.hbox2.addWidget(self.btn)
+        self.hbox2.addWidget(self.list)
+        self.hbox2.addStretch()
+
+        self.vbox.addStretch()        
+        self.vbox.addLayout(self.hbox)
+        self.vbox.addLayout(self.hbox2)
+        self.vbox.addStretch()
+
+        self.btn.clicked.connect(self.browse)
+
+        self.setLayout(self.vbox)        
         self.show()
+
+    def browse(self): 
+        self.file = QFileDialog.getOpenFileNames(self, 'Open file', 'c\\')
+        items = self.file[0]
+        print(items)
+        for photo in items:
+            print(photo)
+            self.list.addItem(photo)
+        return items
+
+    def deleteitem(self):
+        
+        pass
+
+        
